ecc3ade8beff9a029cb7454115564715df86c48e
ThakurAnkur <thakur.ankur049@gmail.com>
2022-06-25 20:14:35 +0530

    feat: removed dependency

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,16 +1,6 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
-import { User } from './entities/user.entity';
 
 @Controller('user')
 export class UserController {
