c9d3330c62df0cbd690c58b84a4608dd41eb0060
ThakurAnkur <thakur.ankur049@gmail.com>
2022-07-04 17:43:29 +0530

    removing unwanted dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import configuration from './config/configuration';
 import { AltTestlibModule } from '@app/alt-testlib';
 import { AltAuthModule } from '@alt/alt-auth';
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      load: [configuration],
       envFilePath: ['.env'],
     }),
     TypeOrmModule.forRoot({
--- src/utils/randomString.ts
+++
@@ -1,8 +0,0 @@
-export const randomString = (len: number) => {
-  const str = 'abcdefghijklmnopqrstuvwxyz1234567890';
-  let randomStr = '';
-  for (let i = 0; i < len; i++) {
-    randomStr = randomStr + str[Math.floor(Math.random() * str.length)];
-  }
-  return randomStr;
-};
